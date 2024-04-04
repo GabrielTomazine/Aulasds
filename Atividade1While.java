5eb64726371c22eee588fb13b78615613feb610e
GabrielTomazine <146785078+GabrielTomazine@users.noreply.github.com>
2024-04-04 14:37:40 -0300

    Add files via upload

---
+++ Atividade1While.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade.pkg1.pkgwhile;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class Atividade1While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 1;
+        System.out.println("Números ímpares de 1 a 100:");
+        while (num <= 100) {
+            System.out.println(num);
+            num += 2;
+        }
+        
+    
+    }
+    
+}   
