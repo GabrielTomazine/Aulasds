2c02d3108f7405c03a9ef40d236775787f23f050
GabrielTomazine <146785078+GabrielTomazine@users.noreply.github.com>
2024-04-18 14:45:22 -0300

    Add files via upload

---
+++ AtividadeLoop.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade.loop;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class AtividadeLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 1;
+        
+        while (num <= 100){
+        
+        System.out.println(num);
+        num++;
+        }
+    }
+    
+}
